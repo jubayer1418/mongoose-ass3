4d7bf730a5a6ac11d1d7a7399056e65cc518d14d
jubayer1418 <skjubayer1418@gmail.com>
2025-08-18 07:46:58 +0600

    fix

--- dist/server.js
+++ dist/server.js
@@ -6,20 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./config"));
-let server;
-const PORT = config_1.default.port;
 async function main() {
     try {
         await mongoose_1.default.connect(config_1.default.database);
-        console.log("Connected to MongoDB Using Mongoose!!");
-        server = app_1.default.listen(PORT, () => {
-            console.log(`App is listening on port ${PORT}`);
+        app_1.default.listen(config_1.default.port, () => {
+            console.log(`Example app listening on port ${config_1.default.port}`);
         });
-        return server;
     }
     catch (error) {
         console.log(error);
-        process.exit(1);
     }
 }
 main();
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,15 @@
-import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
 
-let server: Server;
-
-const PORT = config.port;
-
 async function main() {
   try {
-    await mongoose.connect(config.database!);
-    console.log("Connected to MongoDB Using Mongoose!!");
-    server = app.listen(PORT, () => {
-      console.log(`App is listening on port ${PORT}`);
+    await mongoose.connect(config.database as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
-    return server;
   } catch (error) {
     console.log(error);
-    process.exit(1);
   }
 }
-
 main();
